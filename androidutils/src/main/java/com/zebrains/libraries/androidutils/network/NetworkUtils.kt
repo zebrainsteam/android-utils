08134cb51d81b4f3932bfb3037bc4e76e990b995
Arseniy <40274407+SentinOS@users.noreply.github.com>
2021-06-08 16:26:20 +0400

    NetworkUtils.kt refactored

--- androidutils/src/main/java/com/zebrains/libraries/androidutils/network/NetworkUtils.kt
+++ androidutils/src/main/java/com/zebrains/libraries/androidutils/network/NetworkUtils.kt
@@ -8,22 +8,17 @@ import com.zebrains.libraries.androidutils.Const.WIFI
 
 //TODO: Посмотреть, как можно сократить запись и выделить в отдельную функцию
 
-fun Context.isNetworkConnected(): Boolean {
-    val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-    val activeNetwork = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork) ?: return false
-    return when {
-        activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-        activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-        else -> false
-    }
-}
-
 fun Context.getNetworkType(): String {
     val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-    val activeNetwork = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork) ?: return ""
+    val activeNetwork =
+        connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork) ?: return ""
     return when {
         activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> WIFI
         activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> MOBILE
         else -> ""
     }
+}
+
+fun Context.isNetworkConnected(): Boolean {
+    return getNetworkType().isNotEmpty()
 }
